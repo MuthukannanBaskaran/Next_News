26c93b2905abbce2338e73a4260f1f07005a2f7f
Muthukannan Baskaran <muthukannanbaskaran@gmail.com>
2026-04-27 15:00:01 +0530

    Router added

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Header } from '../components/header/Header';
+import { Home } from '../pages/home/Home';
+import { Search } from '../pages/search/Search';
+import { Categories } from '../pages/categories/Categories';
 
 export const AppRouter = () => {
     return (
-        <div>AppRouter</div>
+        <div>
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/search" element={<Search />} />
+                    <Route path="/categories" element={<Categories />} />
+                </Routes>
+            </BrowserRouter>
+        </div >
     )
 }
 
